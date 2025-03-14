f991742d25a1f5c47326b2cf6c8494acc280b219
Lisuko Лисуко <liza.shevchuk08@gmail.com>
2025-03-14 08:39:30 +0200

    Add project files.

---
+++ Lab2Lib/TV.cs
@@ -0,0 +1,29 @@
+﻿using System.Formats.Asn1;
+
+namespace Lab2Lib
+{
+     public class TV
+    {
+        public string model;
+        public string brand;
+        public int screenSize;
+        public string resolution;
+        public string isSmart;
+        public string color;
+        public float price;
+        public float area
+        {
+            get
+            {
+                double x = Math.Sqrt(screenSize*screenSize/337);
+                return Convert.ToSingle( 16 * 9 * x * x);
+            }
+        }
+
+        public void ChangePrice(float newPrice)
+        {
+            if (newPrice >= 0)
+                price = newPrice;
+        }
+    }
+}
---
+++ Lab3App/Program.cs
@@ -0,0 +1,54 @@
+﻿using Lab2Lib;
+using System.Net.Http.Headers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Введіть кількість телевізорів:");
+        int cntTv = int.Parse(Console.ReadLine());
+        TV[]arrTV = new TV[cntTv];
+        for (int i = 0; i < cntTv; i++)
+        {
+            Console.WriteLine("Введіть модель телевізора: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Введіть бренд телевізора: ");
+            string brand = Console.ReadLine();
+            Console.WriteLine("Введіть розмір телевізора: ");
+            int screenSize = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть розширення телевізора: ");
+            string resolution = Console.ReadLine();
+            Console.WriteLine("Введіть телевізор смарт так/ні: ");
+            string isSmart = Console.ReadLine();
+            Console.WriteLine("Введіть колір телевізора: ");
+            string color = Console.ReadLine();
+            Console.WriteLine("Введіть ціну телевізора: ");
+            float price = float.Parse(Console.ReadLine());
+            TV tv = new TV();
+            tv.model = model;
+            tv.brand = brand;
+            tv.screenSize = screenSize;
+            tv.resolution = resolution;
+            tv.isSmart = isSmart;
+            tv.color = color;
+            tv.price = price;
+
+            arrTV[i] = tv;
+        }
+        foreach (TV tv in arrTV) 
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Дані про телевізор моделі{0}", tv.model);
+            Console.WriteLine("Бренд{0}", tv.brand);
+            Console.WriteLine("Розмір екрану{0}", tv.screenSize);
+            Console.WriteLine("Розширення екрану{0}: ", tv.resolution);
+            Console.WriteLine(tv.isSmart == "так" ? "є смарт": "не має смарту" );
+            Console.WriteLine("Колір{0}", tv.color);
+            Console.WriteLine("Ціна{0}", tv.price);
+            Console.WriteLine("Площа екрану{0}", tv.area);
+
+        }
+    }
+}
